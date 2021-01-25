44d1adc56eda62f65348bf83637e71ff8cc7c060
imelker <l2pickn@yandex.ru>
2021-01-25 21:10:02 +0300

    add daemon mode

--- main.cpp
+++ main.cpp
@@ -20,7 +20,33 @@ int main(int argc, const char* const* argv) {
     Options options{APP_NAME};
     options.addOption<std::string>("config", "c", "Config filepath", "config.toml");
     options.addOption<bool>("daemon", "d", "Daemon mode", "false");
+    options.addOption<bool>("trace-exit-code", "t", "Trace exit code for daemon mode", "false");
+    options.addOption<std::string>("pid-file-name", "p", "Filepath for process ID output", "/var/run/dbgateway.pid");
     options.parse(argc, argv);
+
+#ifndef WIN32
+    auto daemon = options.getValue<bool>("daemon");
+    auto traceExitCode = options.getValue<bool>("trace-exit-code");
+    auto pidFilename = options.getValue<std::string>("pid-file-name");
+    if (daemon) {
+        int r = fork();
+        if (r == 0) {
+            FILE *pidFile = fopen(pidFilename.c_str(), "w+");
+            fprintf(pidFile, "%d", getpid());
+            fclose(pidFile);
+        } else {
+            if (r > 0) {
+                if (traceExitCode) {
+                    int status;
+                    waitpid(r, &status, 0);
+                    printf("Exit code is %d\n", status);
+                }
+                return 0;
+            } else
+                puts("Can't start as a daemon\n");
+        }
+    }
+#endif
 
     Config config{options.getValue<std::string>("config")};
 
@@ -35,7 +61,6 @@ int main(int argc, const char* const* argv) {
 
     Gateway dbGateway(config);
 
-    bool daemon = options.getValue<bool>("daemon");
     while (!SysSignal::serviceTerminated()) {
         std::this_thread::sleep_for(std::chrono::seconds{1});
     }
